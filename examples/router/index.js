0c3941f09cc532713091e20db5f5b4f1a93a64e2
WsmDyj <972774037.com>
2021-04-28 15:29:26 +0800

    fix: router history

--- examples/router/index.js
+++ examples/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { RootRoute } from './modules/basic'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     ...RootRoute,
     {
